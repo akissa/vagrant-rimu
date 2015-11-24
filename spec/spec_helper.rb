6d16bd320df6f93bf7f86de779e601e6846335c4
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-24 12:29:12 +0200

    * Added coveralls * Configure rspec * Require all files under lib/ to get coverage info

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,31 @@
 # require "codeclimate-test-reporter"
 # CodeClimate::TestReporter.start
 require 'simplecov'
-SimpleCov.start
+require 'coveralls'
+
 if ENV['CI']=='true'
   require 'codecov'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
+
+SimpleCov.start
+
 require 'rubygems'
 require 'rspec'
+require 'rspec/its'
+
+Dir['lib/**/*.rb'].each do|file|
+  require_string = file.match(/lib\/(.*)\.rb/)[1]
+  require require_string
+end
+
+I18n.load_path << File.expand_path('locales/en.yml', Pathname.new(File.expand_path('../../', __FILE__)))
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
+end
