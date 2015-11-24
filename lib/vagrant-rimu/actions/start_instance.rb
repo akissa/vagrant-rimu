fcef8ca02aea13933d3fde2e1d79992240c6a994
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-24 17:35:44 +0200

    * Move into actions module * Fix the key error on exception

--- lib/vagrant-rimu/actions/start_instance.rb
+++ lib/vagrant-rimu/actions/start_instance.rb
@@ -2,25 +2,27 @@ require 'log4r'
 
 module VagrantPlugins
   module Rimu
-    class StartInstance
-      def initialize(app, env)
-        @app = app
-        @client = env[:rimu_api]
-        @machine = env[:machine]
-        @logger = Log4r::Logger.new("vagrant_rimu::action::start_instance")
-      end
-
-      def call(env)
-        env[:ui].info I18n.t('vagrant_rimu.starting')
-        begin
-          result = @client.servers.start(@machine.id.to_i)
-          raise StandardError, "No response from the API" if result.nil?
-          raise StandardError, "VPS is not be running" if result.running_state != 'RUNNING'
-        rescue StandardError => e
-          raise Errors::ApiError, "#{e}"
+    module Actions
+      class StartInstance
+        def initialize(app, env)
+          @app = app
+          @client = env[:rimu_api]
+          @machine = env[:machine]
+          @logger = Log4r::Logger.new("vagrant_rimu::action::start_instance")
         end
-        env[:ui].info(I18n.t("vagrant_rimu.ready"))
-        @app.call(env)
+
+        def call(env)
+          env[:ui].info I18n.t('vagrant_rimu.starting')
+          begin
+            result = @client.servers.start(@machine.id.to_i)
+            raise StandardError, "No response from the API" if result.nil?
+            raise StandardError, "VPS is not be running" if result.running_state != 'RUNNING'
+          rescue StandardError => e
+            raise Errors::ApiError, {:stderr=>e}
+          end
+          env[:ui].info(I18n.t("vagrant_rimu.ready"))
+          @app.call(env)
+        end
       end
     end
   end
