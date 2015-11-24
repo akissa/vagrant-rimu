c6c5a44bef274728cc854ab56efa07fb00f0a364
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-24 12:30:24 +0200

    * Rubocop

--- lib/vagrant-rimu.rb
+++ lib/vagrant-rimu.rb
@@ -5,7 +5,7 @@ require 'vagrant-rimu/plugin'
 module VagrantPlugins
   module Rimu
     lib_path = Pathname.new(File.expand_path("../vagrant-rimu", __FILE__))
-    autoload :Errors, lib_path.join("errors")
+    autoload :Errors, lib_path.join('errors')
 
     def self.source_root
       @source_root ||= Pathname.new(File.expand_path("../../", __FILE__))
