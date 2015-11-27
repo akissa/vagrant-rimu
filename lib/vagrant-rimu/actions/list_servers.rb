fd454fee181320bedee73a1b522c053998fc4dad
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-27 14:41:24 +0200

    * Refactored code

--- lib/vagrant-rimu/actions/list_servers.rb
+++ lib/vagrant-rimu/actions/list_servers.rb
@@ -8,9 +8,11 @@ module VagrantPlugins
         end
 
         def call(env)
-          env[:ui].info '%-8s %-30s %-20s %-10s %-9s' % ['ID', 'Hostname', 'Data Centre', 'Host Server', 'Status']
+          heading = '%-8s %-20s %-20s %-15s %-15s' % ['ID', 'Hostname', 'Data Centre', 'Host Server', 'Status']
+          env[:ui].info heading
           @client.orders.orders.each do |o|
-            env[:ui].info '%-8s %-30s %-20s %-10s %-9s' % [o.order_oid, o.domain_name, o.location["data_center_location_code"], o.host_server_oid, o.running_state]
+            row = '%-8s %-20s %-20s %-15s %-15s' % [o.order_oid, o.domain_name, o.location["data_center_location_code"], o.host_server_oid, o.running_state]
+            env[:ui].info row
           end
           @app.call(env)
         end
