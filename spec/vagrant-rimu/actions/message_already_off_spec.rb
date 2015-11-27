4f625f9cf1862e6d75e93651152ae8e120d8ed90
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-27 08:31:01 +0200

    * Added message_already_off tests

---
+++ spec/vagrant-rimu/actions/message_already_off_spec.rb
@@ -0,0 +1,33 @@
+require 'ostruct'
+require 'pathname'
+
+require 'spec_helper'
+
+# action_root = Pathname.new(File.expand_path('../../../../lib/vagrant-rimu/actions', __FILE__))
+# autoload :StopInstance, action_root.join('stop_instance')
+
+describe VagrantPlugins::Rimu::Actions::MessageAlreadyOff do  
+  let(:env) do
+    {}.tap do |env|
+      env[:ui] = double('ui').tap do |ui|
+        ui.stub(:info).with(anything)
+        ui.stub(:error).with(anything)
+      end
+    end
+  end
+
+  let(:app) do
+    double('app').tap do |app|
+      app.stub(:call).with(anything)
+    end
+  end
+
+  describe 'call' do
+    it 'sets info variable' do
+      expect(I18n).to receive(:t).with('vagrant_rimu.already_status', :status => :off)
+      expect(app).to receive(:call)
+      @action = VagrantPlugins::Rimu::Actions::MessageAlreadyOff.new(app, env)
+      @action.call(env)
+    end
+  end
+end
