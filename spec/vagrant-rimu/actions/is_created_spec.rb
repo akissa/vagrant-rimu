a494b77bf13a387ee3d4168e2887d29d5d076d6a
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-27 17:04:23 +0200

    * Added is_created tests

---
+++ spec/vagrant-rimu/actions/is_created_spec.rb
@@ -0,0 +1,49 @@
+require 'pathname'
+require 'spec_helper'
+
+# action_root = Pathname.new(File.expand_path('../../../../lib/vagrant-rimu/actions', __FILE__))
+# autoload :StopInstance, action_root.join('stop_instance')
+
+describe VagrantPlugins::Rimu::Actions::IsCreated do
+  let(:state) { double('state') }
+  let(:machine) { double('machine') }
+
+  let(:env) do
+    {}.tap do |env|
+      env[:ui] = double('ui').tap do |ui|
+        ui.stub(:info).with(anything)
+        ui.stub(:error).with(anything)
+      end
+      machine.stub(:state) { state }
+      env[:machine] = machine
+    end
+  end
+
+  let(:app) do
+    double('app').tap do |app|
+      app.stub(:call).with(anything)
+    end
+  end
+
+  describe 'call' do
+    context 'when server is created' do
+      it 'returns true' do
+        env[:machine].state.stub(:id) { :stopped }
+        expect(app).to receive(:call)
+        @action = VagrantPlugins::Rimu::Actions::IsCreated.new(app, env)
+        @action.call(env)
+        env[:result].should == true
+      end
+    end
+    
+    context 'when server is not created' do
+      it 'returns false' do
+        env[:machine].state.stub(:id) { :not_created }
+        expect(app).to receive(:call)
+        @action = VagrantPlugins::Rimu::Actions::IsCreated.new(app, env)
+        @action.call(env)
+        env[:result].should == false
+      end
+    end
+  end
+end
