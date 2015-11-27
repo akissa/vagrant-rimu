2aee2477292c8ee48f02b8f586213ddc4c8634bb
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-27 22:29:46 +0200

    * Moved the public_key function

--- lib/vagrant-rimu/actions/setup_user.rb
+++ lib/vagrant-rimu/actions/setup_user.rb
@@ -48,7 +48,7 @@ module VagrantPlugins
           path = @machine.config.ssh.private_key_path
           path = path[0] if path.is_a?(Array)
           path = File.expand_path(path, @machine.env.root_path)
-          pub_key = Rimu.public_key(path)
+          pub_key = public_key(path)
           @machine.communicate.execute(<<-BASH)
             if ! grep '#{pub_key}' /home/#{user}/.ssh/authorized_keys; then
               echo '#{pub_key}' >> /home/#{user}/.ssh/authorized_keys;
@@ -61,6 +61,12 @@ module VagrantPlugins
           @machine.config.ssh.username = user
 
           @app.call(env)
+        end
+        
+        def public_key(private_key_path)
+          File.read("#{private_key_path}.pub")
+        rescue
+          raise Errors::PublicKeyError, :path => "#{private_key_path}.pub"
         end
       end
     end
