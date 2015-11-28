09b489492bbb72f3eecf05120047fe1881639842
Andrew Colin Kissa <andrew@topdog.za.net>
2015-11-28 12:30:40 +0200

    * Fix client nil issue * Improved the output

--- lib/vagrant-rimu/actions/billing_methods.rb
+++ lib/vagrant-rimu/actions/billing_methods.rb
@@ -2,15 +2,14 @@ module VagrantPlugins
   module Rimu
     module Actions
       class BillingMethods
-        def initialize(app, env)
+        def initialize(app, _env)
           @app = app
-          @client = env[:rimu_api]
         end
 
         def call(env)
-          env[:ui].info '%-6s %-6s %s' % ['ID', 'Type', 'Description']
-          @client.billing_methods.each do |b|
-            env[:ui].info '%-6s %-6s %s' % [b.billing_oid, b.billing_method_type, b.description]
+          env[:ui].info '%-20s %-20s %s' % ['ID', 'Type', 'Description']
+          env[:rimu_api].billing_methods.each do |b|
+            env[:ui].info '%-20s %-20s %s' % [b.billing_oid, b.billing_method_type, b.description]
           end
           @app.call(env)
         end
